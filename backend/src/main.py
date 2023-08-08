ffaa54d99b36907d6bd8a4e1c4f6f83e2fd4d174
Sharp <59743251+chae03yb@users.noreply.github.com>
2023-08-08 13:58:49 +0900

    Flask -> quart 변경

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,1 +1,11 @@
-import flask
+import quart
+import pymysql
+
+# Flask와 동일하게 작성하면 됨.
+app = quart.Quart(__name__)
+
+@app.route("/")
+async def hi():
+    return "Hello, world!"
+
+app.run(port=8000)
