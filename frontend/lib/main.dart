ae45517b9b88a1f9571d03ba4b970c0ea1a92edd
chae03yb <chae03yb@gmail.com>
2023-08-11 22:50:24 +0900

    메인 화면 개발중

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,9 +1,14 @@
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
+
+import "page_main/p_main.dart";
 
 int currentPageIndex = 0;
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((v) => runApp(const MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -16,67 +21,69 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
     final controller = PageController(initialPage: currentPageIndex);
 
     void update() {
       setState(() {});
     }
 
-    // TODO: PageView & BottomNavigationBar 상태 동기화 함수 작성 필요
     return MaterialApp(
         title: "오성고 급식 앱",
         home: Scaffold(
           body: Column(
-            children: [
-              Expanded(
-                  child: PageView(
-                controller: controller,
-                onPageChanged: (index) {
-                  setState(() {
-                    currentPageIndex = index;
-                  });
-                  update();
-                },
-                children: const [
-                  Center(
-                    child: Text("Home"),
-                  ),
-                  Center(
-                    child: Text("Individual"),
-                  ),
-                  Center(
-                    child: Text("Group"),
-                  ),
-                  Center(
-                    child: Text("Settings"),
-                  ),
-                ],
-              ))
-            ],
+            children: [buildPageView(controller, update)],
           ),
-          bottomNavigationBar: BottomNavigationBar(
-              type: BottomNavigationBarType.fixed,
-              backgroundColor: Colors.blue,
-              currentIndex: currentPageIndex,
-              selectedItemColor: Colors.amber,
-              unselectedItemColor: Colors.white12,
-              onTap: (index) {
-                setState(() {
-                  currentPageIndex = index;
-                });
-                controller.jumpToPage(index);
-                update();
-              },
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.person), label: "individual"),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.group), label: "group"),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.settings), label: "settings"),
-              ]),
+          bottomNavigationBar: buildBottomNavigationBar(controller, update),
         ));
+  }
+
+  BottomNavigationBar buildBottomNavigationBar(
+      PageController controller, void Function() update) {
+    return BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.blue,
+        currentIndex: currentPageIndex,
+        selectedItemColor: Colors.amber,
+        unselectedItemColor: Colors.white12,
+        onTap: (index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+          controller.jumpToPage(index);
+          update();
+        },
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), label: "individual"),
+          BottomNavigationBarItem(icon: Icon(Icons.group), label: "group"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: "settings"),
+        ]);
+  }
+
+  Expanded buildPageView(PageController controller, void Function() update) {
+    return Expanded(
+        child: PageView(
+      controller: controller,
+      onPageChanged: (index) {
+        setState(() {
+          currentPageIndex = index;
+        });
+        update();
+      },
+      children: const [
+        MainPageWidget(),
+        Center(
+          child: Text("Individual"),
+        ),
+        Center(
+          child: Text("Group"),
+        ),
+        Center(
+          child: Text("Settings"),
+        ),
+      ],
+    ));
   }
 }
---
+++ frontend/lib/page_main/p_main.dart
@@ -0,0 +1,34 @@
+import "package:flutter/material.dart";
+
+class MainPageWidget extends StatelessWidget {
+  const MainPageWidget({
+    super.key
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(padding: EdgeInsets.all(50)),
+        Center(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Text("남은 대기 인원: nn", style: TextStyle(fontSize: 20),),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: Divider(
+            height: 1,
+            thickness: 1,
+            indent: 20,
+            endIndent: 20,
+            color: Colors.black38,
+          ),
+        ),
+        Expanded(child: Placeholder())
+      ],
+    );
+  }
+}
