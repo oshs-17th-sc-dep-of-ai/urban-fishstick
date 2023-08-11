ed93bf39c59c4e51522f7b7e491ba75a6d3e8ef5
chae03yb <chae03yb@gmail.com>
2023-08-11 14:36:03 +0900

    페이지 요소 배치 필요

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -6,97 +6,77 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final controller = PageController(initialPage: currentPageIndex);
 
+    void update() {
+      setState(() {});
+    }
+
     // TODO: PageView & BottomNavigationBar 상태 동기화 함수 작성 필요
     return MaterialApp(
-      title: "오성고 급식 앱",
-      home: Scaffold(
-        body: Column(
-          children: [
-            PageViewWidget(controller: controller, )
-          ],
-        ),
-        bottomNavigationBar: const NavigationBarWidget(),
-      )
-    );
-  }
-}
-
-class PageViewWidget extends StatelessWidget {
-  const PageViewWidget({
-    super.key,
-    required this.controller,
-    // required this.synchronizer
-  });
-
-  final PageController controller;
-  // final void Function(int)? synchronizer;
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: PageView(
-        controller: controller,
-        // onPageChanged: synchronizer,
-        children: [
-          Center(child: Text("Home"),),
-          Center(child: Text("Individual"),),
-          Center(child: Text("Group"),),
-          Center(child: Text("Settings"),),
-        ],
-      ),
-    );
-  }
-}
-
-class NavigationBarWidget extends StatefulWidget {
-  const NavigationBarWidget({super.key});
-
-  @override
-  State<NavigationBarWidget> createState() => _NavigationBarWidget();
-}
-
-class _NavigationBarWidget extends State<NavigationBarWidget> {
-  void _onItemTapped(int index) {
-    setState(() {
-      currentPageIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      backgroundColor: Colors.blue,
-      currentIndex: currentPageIndex,
-      selectedItemColor: Colors.amber,
-      unselectedItemColor: Colors.white12,
-      onTap: _onItemTapped,
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: "home"
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person),
-          label: "individual"
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.group),
-          label: "group"
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.settings),
-          label: "settings"
-        )
-      ]
-    );
+        title: "오성고 급식 앱",
+        home: Scaffold(
+          body: Column(
+            children: [
+              Expanded(
+                  child: PageView(
+                controller: controller,
+                onPageChanged: (index) {
+                  setState(() {
+                    currentPageIndex = index;
+                  });
+                  update();
+                },
+                children: const [
+                  Center(
+                    child: Text("Home"),
+                  ),
+                  Center(
+                    child: Text("Individual"),
+                  ),
+                  Center(
+                    child: Text("Group"),
+                  ),
+                  Center(
+                    child: Text("Settings"),
+                  ),
+                ],
+              ))
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: Colors.blue,
+              currentIndex: currentPageIndex,
+              selectedItemColor: Colors.amber,
+              unselectedItemColor: Colors.white12,
+              onTap: (index) {
+                setState(() {
+                  currentPageIndex = index;
+                });
+                controller.jumpToPage(index);
+                update();
+              },
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.person), label: "individual"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.group), label: "group"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.settings), label: "settings"),
+              ]),
+        ));
   }
 }
