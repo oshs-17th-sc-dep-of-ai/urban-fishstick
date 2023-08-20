0c24cb66d941acffc16f0a7e7b07f13ce5707c1f
Yusanggyu <ysk060915@gmail.com>
2023-08-20 17:52:18 +0900

    통신 구현중

--- backend/server/main.py
+++ backend/server/main.py
@@ -3,14 +3,24 @@ from flask import Blueprint, jsonify, Flask
 example_bp = Flask(__name__)
 profile = Blueprint("profile", __name__)
 example_bp.register_blueprint(profile, url_prefix='/profile')
-
-@example_bp.route('/send', methods=['GET'])
+count = 0
+@example_bp.route('/send', methods=["POST"])
 def example_route():
-    who = 'JSONdata'
-    apply = 'jsondata'
-    result = {
-        who : apply
+    global count
+    data = {
+        "ID" : 20820,
+        "check" : True
     }
-    return jsonify(result=result)
+
+    user_id = data['ID']
+    check_value = data['check']
+    count += 1
+
+    response_data = {
+        'ID': user_id,
+        'check': check_value,
+        'result': count
+    }
+    return jsonify(response_data)
 if __name__ == '__main__':
     example_bp.run()
