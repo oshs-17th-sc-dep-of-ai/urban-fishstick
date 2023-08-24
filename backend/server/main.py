b71af5634024c69896153f0346906f299df845af
Yusanggyu <134144376+Yusanggyu@users.noreply.github.com>
2023-08-24 00:40:31 +0000

    src 폴더 삭제, 위치 변경, 통신 구현

--- backend/server/main.py
+++ backend/server/main.py
@@ -1,26 +1,27 @@
 from flask import Blueprint, jsonify, Flask
+import deque
 
 example_bp = Flask(__name__)
 profile = Blueprint("profile", __name__)
 example_bp.register_blueprint(profile, url_prefix='/profile')
-count = 0
-@example_bp.route('/send', methods=["POST"])
+@example_bp.route('/', methods=["GET"])
 def example_route():
-    global count
+    학번 = 20820 # int
+    확인 = True # bool
     data = {
-        "ID" : 20820,
-        "check" : True
+        "ID" : 학번,
+        "check" : 확인
     }
 
     user_id = data['ID']
     check_value = data['check']
-    count += 1
 
-    response_data = {
-        'ID': user_id,
-        'check': check_value,
-        'result': count
-    }
-    return jsonify(response_data)
+    deque.apply_meal(user_id)
+
+    if check_value:
+        apply = deque.apply_meal(user_id)
+        index = deque.index(user_id)
+    response_data = {apply:index}
+    return response_data
 if __name__ == '__main__':
-    example_bp.run()
+    example_bp.run(debug=True)
