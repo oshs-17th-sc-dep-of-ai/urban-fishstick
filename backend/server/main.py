60dad9cd95d4256c0f7db0cdb7614e483ce7edb6
Yusanggyu <ysk060915@gmail.com>
2023-08-25 23:20:56 +0900

    통신 구현 완료, 실패 사유 타폴더에 만들기

--- backend/server/main.py
+++ backend/server/main.py
@@ -6,25 +6,29 @@ profile = Blueprint("profile", __name__)
 example_bp.register_blueprint(profile, url_prefix='/profile')
 @example_bp.route('/', methods=["GET"])
 def example_route():
+    success = False
+    reason = "<Input error>"
     학번 = 20820 # int
-    확인 = True # bool
     data = {
         "ID" : 학번,
-        "check" : 확인
     }
 
     user_id = data['ID']
-    check_value = data['check']
 
     deque.apply_meal(user_id)
 
-    if check_value:
-        apply = deque.apply_meal(user_id)
-        index = deque.index(user_id)
-    response_data = {
-                    "success" : True,
-                    "waiting" : index
-                     }
+    apply = deque.apply_meal(user_id)
+    index = deque.index(user_id)
+    if success:
+        response_data = {
+            "success" : True,
+            "waiting" : index
+                        }
+    else:
+        response_data = {
+            "success" : False,
+            "reason" : reason
+        }
     return response_data
 if __name__ == '__main__':
     example_bp.run(debug=True)
