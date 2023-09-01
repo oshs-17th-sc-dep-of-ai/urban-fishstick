68a771222bb8175c666c00699cc34d85dac4ea2d
Yusanggyu <134144376+Yusanggyu@users.noreply.github.com>
2023-09-01 12:12:46 +0000

    3학년 제한 구현 / 시간 추가해야됨

--- backend/server/main.py
+++ backend/server/main.py
@@ -8,10 +8,12 @@ example_bp.register_blueprint(profile, url_prefix='/profile')
 def example_route():
     success = False
     reason = "<Input error>"
-    학번 = 20820 # int
+    학번 = [30820] # int
     data = {
         "ID" : 학번,
     }
+    if data['ID']: pass
+
 
     user_id = data['ID']
 
@@ -19,15 +21,25 @@ def example_route():
 
     apply = deque.apply_meal(user_id)
     index = deque.index(user_id)
+
+    reason = {
+        "grade" : "Cannot enter",
+        "input" : "Input error"
+    }
     if success:
         response_data = {
             "success" : True,
             "waiting" : index
                         }
-    else:
+    elif filter(lambda f: f < 30000, data["ID"]):
         response_data = {
             "success" : False,
-            "reason" : reason
+            "reason" : reason["grade"]
+        }
+    else:
+        response_data = {
+            "successs" : False,
+            "reason" : reason["input"]
         }
     return response_data
 if __name__ == '__main__':
