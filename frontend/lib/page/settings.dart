ead3bc20419cb1f411d5236d19189c67f30547d5
Woochan0510 <8621skyblue@naver.com>
2023-09-17 21:00:19 +0900

    설정칸 각 항목별 박스 추가 및 타이틀 지정 / 배경색 변경

--- frontend/lib/page/settings.dart
+++ frontend/lib/page/settings.dart
@@ -5,6 +5,45 @@ class SettingsPageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: 700,
+                height: 100,
+                color: Colors.red,
+                child: const Text(
+                  "알레르기",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                width: 700,
+                height: 100,
+                color: Colors.blue,
+                child: const Text(
+                  "건의/버그",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                width: 700,
+                height: 100,
+                color: Colors.pink,
+                child: const Text(
+                  "라이센스",
+                  textAlign: TextAlign.center,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
