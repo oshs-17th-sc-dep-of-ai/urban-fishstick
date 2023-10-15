932aaba6f56d07d38fcb6d0295f542eeb4308d11
Woochan0510 <8621skyblue@naver.com>
2023-10-15 17:39:40 +0900

    건의/버그 파일 추가

---
+++ frontend/lib/page/bug.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+class LicensePageWidget extends StatelessWidget {
+  const LicensePageWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
