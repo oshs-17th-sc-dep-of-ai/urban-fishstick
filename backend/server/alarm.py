04e7516ff35382be5e908d99a774c90550782b2b
Yusanggyu <134144376+Yusanggyu@users.noreply.github.com>
2023-10-15 10:27:15 +0000

    backend 코드정리

--- backend/server/alarm.py
+++ backend/server/alarm.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, jsonify, Flask
 import deque
-ALARM_LEFT_LINE =5 # 대기줄 내려옴 알림
-example_bp = Flask(__name__)
-@example_bp.route('/', methods=["get"]) # codespace에서 예외파일 실행 안됨, 이후 url 변경 (url = /alarm)
+ALARM_LEFT_LINE = 5 # 대기줄 내려옴 알림
+waiting = Flask(__name__)
+@waiting.route('/', methods=["get"]) # codespace에서 예외파일 실행 안됨, 이후 url 변경 (url = /alarm)
 def alarm():
     result = {
         "alar" : "come down",
-        "group" : deque.q[ALARM_LEFT_LINE]
+        "group" : deque.q[0]
     }
-    return result
+    return jsonify(result)
