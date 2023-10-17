ed90f11d1cdfe50f6b806015745f1416bde2f669
Woochan0510 <8621skyblue@naver.com>
2023-10-17 21:31:46 +0900

    statefulwidget으로 변경

--- frontend/lib/page/bug.dart
+++ frontend/lib/page/bug.dart
@@ -1,8 +1,13 @@
 import "package:flutter/material.dart";
 
-class LicensePageWidget extends StatelessWidget {
-  const LicensePageWidget({super.key});
+class BugPageWidget extends StatefulWidget {
+  const BugPageWidget({super.key});
 
+  @override
+  State<BugPageWidget> createState() => _BugPageWidgetState();
+}
+
+class _BugPageWidgetState extends State<BugPageWidget> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
--- frontend/lib/page/license.dart
+++ frontend/lib/page/license.dart
@@ -1,8 +1,13 @@
 import "package:flutter/material.dart";
 
-class LicensePageWidget extends StatelessWidget {
+class LicensePageWidget extends StatefulWidget {
   const LicensePageWidget({super.key});
 
+  @override
+  State<LicensePageWidget> createState() => _LicensePageWidgetState();
+}
+
+class _LicensePageWidgetState extends State<LicensePageWidget> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
