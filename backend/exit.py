c4a2501ba2146c87b5cdda8c80b830af7c41928b
jiyouon <141696842+jiyouon@users.noreply.github.com>
2024-02-13 22:06:16 +0900

    exit.py
    수정 완료

--- backend/exit.py
+++ backend/exit.py
@@ -1,10 +1,8 @@
 from flask import Flask, Blueprint, request, jsonify
 
-app = Flask(__name__)
-
 exit_blueprint = Blueprint("exit", __name__)
 
-group_info = []
+group = []
 
 # 퇴장
 @exit_blueprint.route("/exit", methods=["POST"])
@@ -12,13 +10,10 @@ def exit():
     try:
         student_id = request.data.decode("utf-8")
 
-        if student_id in group_info:
-            group_info.remove(student_id)
+        if student_id in group:
+            group.remove(student_id)
             return jsonify({"message": "퇴장 처리 완료"}), 200
         else:
             return jsonify({"message": "해당 학생이 그룹에 존재하지 않습니다."}), 404
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-
-app.register_blueprint(exit_blueprint)
