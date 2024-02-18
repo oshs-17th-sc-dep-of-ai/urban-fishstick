12d24488de6325c1419c20da0bad75076302dc53
Woochan0510 <8621skyblue@naver.com>
2024-02-18 21:59:08 +0900

    라이센스 화면 앱바 제작 내용 추가해야함

--- frontend/lib/page/license.dart
+++ frontend/lib/page/license.dart
@@ -10,6 +10,21 @@ class LicensePageWidget extends StatefulWidget {
 class _LicensePageWidgetState extends State<LicensePageWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 236, 236, 236),
+        title: const Text(
+          '라이센스',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize:30,
+          ),
+        ),
+        centerTitle: true,
+        toolbarHeight: 75,
+        leadingWidth: 70,
+      ),
+    );
   }
+  
 }
