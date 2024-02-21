a9f658ea2da90ef993d6f84b9d7a6fc18a206a41
jcg0615 <gunnexon@gmail.com>
2024-02-21 12:48:58 +0900

    버그페이지 업뎃

--- frontend/lib/page/bug.dart
+++ frontend/lib/page/bug.dart
@@ -8,8 +8,28 @@ class BugPageWidget extends StatefulWidget {
 }
 
 class _BugPageWidgetState extends State<BugPageWidget> {
+  final myController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+            margin: EdgeInsets.all(8),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(20), hintText: '건의사항'),
+              controller: myController,
+            )),
+        FloatingActionButton(
+            child: Text("제출"),
+            onPressed: () => showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(content: Text("의견 감사합니다!"));
+                }))
+      ],
+    );
   }
 }
