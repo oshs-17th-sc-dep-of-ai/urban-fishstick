c16ebdb3c02f269cca81b60a06918653c6c6a70d
신우찬 <8621skyblue@naver.com>
2024-11-08 01:48:42 +0900

    notification이 초기화될때까지 기다리도록 변경

--- frontend/lib/util/notification.dart
+++ frontend/lib/util/notification.dart
@@ -6,6 +6,8 @@ class FNotification {
   static FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
 
+  static bool _isInitialized = false; // 초기화 상태를 추적하는 변수
+
   static init() async {
     // TODO: iOS 설정 초기화
     AndroidInitializationSettings androidInitializationSettings =
@@ -15,9 +17,16 @@ class FNotification {
         InitializationSettings(android: androidInitializationSettings);
 
     await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+    _isInitialized = true; // 초기화가 완료되면 true로 설정
   }
 
   static Future<void> showNotification(String title, String body) async {
+    // 초기화가 완료되지 않으면 기다린다
+    if (!_isInitialized) {
+      await Future.delayed(Duration(seconds: 1)); // 잠시 대기
+      return showNotification(title, body); // 다시 호출
+    }
+
     const AndroidNotificationDetails androidNotificationDetails =
         AndroidNotificationDetails("os_lunch", "입장 알림",
             channelDescription: "입장 순서 알림",
